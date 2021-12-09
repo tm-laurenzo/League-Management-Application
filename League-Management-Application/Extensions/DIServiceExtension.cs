ecde2db022852dc2796b4616625b04afd6f3c392
tm-laurenzo <Chibuike.Chidolue@decagon.dev>
2021-12-09 21:06:19 +0100

    added the class library for data and database operations

---
+++ League-Management-Application/Extensions/DIServiceExtension.cs
@@ -0,0 +1,26 @@
+
+
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace League_Management_Application.Extensions
+{
+    public static class DIServiceExtension
+    {
+        public static void AddDependencyInjection (this IServiceCollection services)
+        {
+            // Add Service Injections Here
+           
+
+            // Add Repository Injections Here
+            
+            // Add Model Services Injection Here
+
+           
+
+            // Add Fluent Validator Injections Here
+           
+        }
+    }
+}
+
---
+++ League-Management-Data/Context/LMADbContext.cs
@@ -0,0 +1,32 @@
+﻿using League_Management_Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace League_Management_Data.Context
+{
+    public class LMADbContext : IdentityDbContext<User>
+    {
+        public LMADbContext(DbContextOptions<LMADbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Agent> Agents { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Goal> Goals { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<MatchStatistics> MatchStatistics { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Records> Records { get; set; }
+        public DbSet<Refree> Refrees { get; set; }
+        public DbSet<Team> Teams { get; set; }
+
+    }
+}
