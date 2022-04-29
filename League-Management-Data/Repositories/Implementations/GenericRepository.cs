d4a4e327d632019c1ddb6f615c0b79330c98f1a7
tm-laurenzo <t.m.laurenzo@gmail.com>
2022-04-29 22:39:13 +0100

    Added Agent, Generic, Manger, Owner, Player  repositories and IGenericRepository interface

---
+++ League-Management-Data/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using League_Management_Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace League_Management_Data.Repositories.Implementations
+{
+    public class GenericRepository<T> :  IGenericRepository<T> where T : class
+    {
+        private readonly LMADbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+
+        public GenericRepository(LMADbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+
+
+        }
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+
+        public async Task InsertAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Update(entity);
+        }
+    }
+}
