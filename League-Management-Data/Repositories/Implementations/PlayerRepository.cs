d14b919c74560c6aa3022581f5834c5b82e8fbd2
Chibuike Chidolue <t.m.laurenzo@gmail.com>
2022-05-10 00:18:18 +0100

    fields and contructor for the player repository created

--- League-Management-Data/Repositories/Implementations/PlayerRepository.cs
+++ League-Management-Data/Repositories/Implementations/PlayerRepository.cs
@@ -1,5 +1,7 @@
-﻿using League_Management_Data.Repositories.Implementations;
+﻿using League_Management_Data.Context;
+using League_Management_Data.Repositories.Implementations;
 using League_Management_Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +12,13 @@ namespace League_Management_Data.Repositories
 {
     public class PlayerRepository : GenericRepository<Player>
     {
+        private readonly LMADbContext _context;
+        private readonly DbSet<Player> _dbSet;
+
+        public PlayerRepository(LMADbContext context) : base(context)
+        {
+            _context = context;
+            _dbSet = _context.Set<Player>();
+        }
     }
 }
