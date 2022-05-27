51dfb16344f0fd69cb5000b58c0a0762bdf4f004
Chibuike Chidolue <t.m.laurenzo@gmail.com>
2022-05-27 23:47:45 +0100

    still working on add new manager method

---
+++ League-Management-Dtos/RequestDTOs/ManagerDTOs/AddManagerDTO.cs
@@ -0,0 +1,23 @@
+﻿using League_Management_Models;
+using System.Collections.Generic;
+
+namespace League_Management_DTOs
+{
+    public class AddManagerDTO
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public User User { get; set; }
+        public string AgentId { get; set; }
+        public Agent Agent { get; set; }
+        public string AccountName { get; set; }
+        public string AccountNumber { get; set; }
+        public string? CurrentTeamId { get; set; }
+       
+        public Team? CurrentTeam { get; set; }
+    
+        public ICollection<Team> ListOfPreviousTeams { get; set; }
+    }
+}
